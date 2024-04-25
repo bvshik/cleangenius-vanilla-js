68e24496ae6c3b33e3b1c5121ff51903c1d3eecf
Bvshik <bushkrzok@gmail.com>
2024-04-25 14:50:47 +0200

    fixed link

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 document.addEventListener("click", (e) => {
   if (!e.target.matches("[login-button]")) return;
-  window.location.replace("cleangenius-projzal-internal.netlify.app");
+  window.location.replace("https://cleangenius-projzal-internal.netlify.app");
 });
