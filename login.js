bb2eacd0b37dc9aac18dadbf6a6103e6558f00a2
Bvshik <bushkrzok@gmail.com>
2024-04-25 14:40:15 +0200

    button transfers to correct page

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.addEventListener('click', e =>{
-    if(!e.target.matches("[login-button]")) return;
-    window.location.replace("https://youtube.com");
-})
+document.addEventListener("click", (e) => {
+  if (!e.target.matches("[login-button]")) return;
+  window.location.replace("cleangenius-projzal-internal.netlify.app");
+});
